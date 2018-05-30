977768f6965a05956ac0d2d7a3e0b36d4762e176
Knoll <noll.kyle@gmail.com>
2018-05-29 23:43:35 -0500

    Image uploading

--- Gemfile
+++ Gemfile
@@ -17,6 +17,9 @@ gem 'jquery-rails'
 gem 'simple_form'
 gem 'devise'
 gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
+gem 'carrierwave', '~> 1.0'
+gem 'figaro'
+gem 'fog-aws'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,19 @@ user = User.create! email: 'noll.kyle@gmail.com',
                     password: 'password',
                     password_confirmation: 'password'
 
-10.times do
+user.courses.create title: "Learn the Rubik's Cube",
+                   description: "Learn how to solve the cube from world-class masters.",
+                   cost: 5
+
+user.courses.create title: "Become a minimalist",
+                   description: "Learn how to declutter your home and live the simple life.",
+                   cost: 5
+
+user.courses.create title: "Guitar Lessons",
+                   description: "Learn how to shred on the guitar like the legends do.",
+                   cost: 5
+
+7.times do
   user.courses.create title:       Faker::Lorem.sentence.chomp('.'),
                       description: Faker::Lorem.paragraph(sentence_count = 5+(rand()*2).to_i),
                       cost:        (rand()*100).to_i
